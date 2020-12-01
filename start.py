33c0b346fa6654104d0feb89c84fa4e31b51764f
TNTrol <tntrol.go@gmail.com>
2020-12-01 04:23:20 +0300

    add scripts

--- start.py
+++ start.py
@@ -5,10 +5,12 @@ import mysql.connector as MySQL
 from mysql.connector import Error
 from user import MyUser
 from flask import json
+import json as my_json
 
 app = Flask(__name__)
 login_manager = LoginManager(app)
 app.config['SECRET_KEY'] = 'a really really really really long secret key'
+
 
 def recover_connection(base_name, user, password, buffered = False):
 	try:
@@ -27,6 +29,10 @@ def get_data_for_js(array):
 	for row in array:
 		outList.append((row[0], str(row[1])))
 	return outList
+
+def select_from(name):
+	cursor.execute('SELECT * FROM ' + name + ';')
+	return cursor.fetchall()
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -50,7 +56,7 @@ def login():
 	if user_sql is None :
 		return render_template('login.html')
 	user = MyUser(user_sql[1], user_sql[2], user_sql[0])
-	login_user( user)		
+	login_user( user)
 	return redirect(url_for('base'))
 
 @app.route('/logout')
@@ -69,28 +75,53 @@ def service(index):
 	response = app.response_class(response=json.dumps(data), status=200, mimetype='application/json')
 	return response
 
+@app.route('/check', methods=["GET", "POST"])
+@login_required
+def generate_airline():
+	return render_template('airline.html')
+
 @app.route('/add', methods=["GET", "POST"])
 @login_required
 def index():
-	
-	if request.method == 'GET':
-		cursor.execute('SELECT ID_AIRLINE,NAME_AIRLINE FROM AIRLINE;')
-		airline = cursor.fetchall()
-		cursor.execute('SELECT * FROM mydb.DISPATCHER;')
-		despatcher = cursor.fetchall()
-		cursor.execute('SELECT * FROM AIRCRAFT;')
-		aircraft = cursor.fetchall()
-		cursor.execute('SELECT * FROM TECH_SERVICE;')
-		tex = cursor.fetchall()
-		cursor.execute('SELECT * FROM EARTH_SERVICE;')
-		ear = cursor.fetchall()
-		return render_template('add.html', airline = airline, despatcher = despatcher,aircraft = aircraft, tex_service = tex )
+	if request.method == 'POST':
+		data = my_json.loads(request.form['data'])
+		airl = data['airline']
+		airc = data['aircraft']
+		print(data)
+		dis = data['dispatcher']
+		arr_types = data['types']
+		arr_service = data['services']
+		try:
+			global id_act
+			cursor.execute('INSERT LANDING_ACT (ID_ACT, DATE,ID_DISPATCHER,ID_AIRLINE,TYPE_AIRCRAFT ) VALUES(?, CURRENT_DATE(), ?, ?, ?);', (id_act, int(dis), int(airl), int(airc)))
+			for i in range(len(arr_types)):
+				cursor.execute('INSERT mydb.LIST_SERVICE (ID_ACT, TYPE_SERVICE, ID_SERVICE) VALUES(?, ?, ?);', (id_act, arr_types[i], int(arr_service[i]) ))
+			cursor.execute(' SELECT SUM(COST) FROM mydb.TARIFF INNER JOIN mydb.LIST_SERVICE ON mydb.LIST_SERVICE.ID_ACT = ? AND mydb.TARIFF.ID_AIRCRAFT = ? WHERE mydb.LIST_SERVICE.TYPE_SERVICE = mydb.TARIFF.TYPE_SERVICE AND mydb.LIST_SERVICE.ID_SERVICE = mydb.TARIFF.ID_SERVICE ;''', (id_act, int(airc)))
+			cost = cursor.fetchall()
+			cursor.execute('INSERT mydb.SCORE (ID_ACT, SCORE) VALUES (?, ?)', (id_act, float(cost[0][0])/2))
+			id_act += 1
+		except Exception as e:
+			db.rollback()
+			print()
+			print(e)
+		else:
+			db.commit();
+	#if request.method == 'GET':
+	airline = select_from('AIRLINE')
+	despatcher = select_from('mydb.DISPATCHER')
+	aircraft = select_from('AIRCRAFT')
+	tex = select_from('TECH_SERVICE')
+	#ear = select_from('EARTH_SERVICE')
+	return render_template('add.html', airline = airline, despatcher = despatcher,aircraft = aircraft, tex_service = tex )
 
 if __name__ == "__main__":
-	db , cursor = recover_connection('mydb', 'tntrol', 'dfkjd', True)
-	app.run(debug = True)
-	airline = cursor.execute('SELECT NAME_AIRLINE	,ID_AIRLINE FROM AIRLINE;')
-	if airline is not None:
-		print("urrr")
+	try:
+		db , cursor = recover_connection('mydb', 'tntrol', 'dfkjd', True)
+		#cursor.execute('SELECT MAX(ID_ACT) FROM LANDING_ACT')
+		#id_act = cursor.fetchall()
+		id_act = 130 #int(id_act[0][0]) + 1
+		app.run(debug = True)
+	except:
+		print('you lose')
 	
 
